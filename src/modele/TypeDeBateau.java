607911c13f76d1f78ca523f83d4713b2d1c82fa8
florian <gigotFlorian@gmail.com>
2020-01-09 17:55:56 +0100

    Enumeration type de bateau

---
+++ src/modele/TypeDeBateau.java
@@ -0,0 +1,21 @@
+package modele;
+
+public enum TypeDeBateau {
+	BateauAVoile("Voilier"),
+	BateauAMoteur("Bateau a moteur");
+	
+	private String typeDeBateau;
+	
+	private TypeDeBateau(String typeDeBateau){
+		this.typeDeBateau = typeDeBateau;
+	}
+
+	public String getTypeDeBateau() {
+		return typeDeBateau;
+	}
+
+	public void setTypeDeBateau(String typeDeBateau) {
+		this.typeDeBateau = typeDeBateau;
+	}
+	
+}
